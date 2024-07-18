50bc4950180a2b00d574d89f3c042e0adde82380
ayazukuri <korscr1027@yahoo.com>
2024-07-18 10:57:50 +0200

    fix: node width unrestricted

--- frontend/src/Node.tsx
+++ frontend/src/Node.tsx
@@ -3,7 +3,7 @@ import { TreeType } from "./Tree";
 function Node(props: { node: TreeType, parent: number, updateState: () => void, selector: () => void, isSelected: boolean }) {
     if (props.node.type === "leaf")
         return <div onClick={props.selector} data-parent={props.parent} className={props.isSelected ? "node node-selected" : "node"} style={{ textAlign: "center", lineHeight: "3" }}>{props.node.diagnosis}</div>
-    return <div onClick={props.selector} data-parent={props.parent} className={props.isSelected ? "node node-selected" : "node"} title={"Threshold: " + props.node.threshold.toString()} style={{ textAlign: "center", lineHeight: "3" }}>{props.node.measurement},{props.node.threshold.toFixed(2)}</div>
+    return <div onClick={props.selector} data-parent={props.parent} className={props.isSelected ? "node node-selected" : "node"} title={"Threshold: " + props.node.threshold.toString()} style={{ textAlign: "center", lineHeight: "3" }}>{props.node.measurement} @ {props.node.threshold.toFixed(2)}</div>
 }
 
 export default Node
